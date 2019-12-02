7ebc633545836773a35466adef62c5ba5f2eb299
Emam <emamAhmed36@gmail.com>
2019-12-02 16:13:49 +0200

    sepratr code into static class

--- app/src/test/java/com/example/hackerrankpractice/ExampleUnitTest.kt
+++ app/src/test/java/com/example/hackerrankpractice/ExampleUnitTest.kt
@@ -12,29 +12,6 @@ class ExampleUnitTest {
 
     @Test
     fun Grading_Students() {
-        assertEquals(arrayOf(75,67,40,33), gradingStudents(arrayOf(73,67,38,33)))
-    }
-
-    fun gradingStudents(grades: Array<Int>): Array<Int> {
-        // Write your code here
-        var finalGrade = grades
-
-        (grades.indices).forEach { i ->
-            var grade = grades[i]
-            if (grade < 38)
-                finalGrade[i] = grade
-            else {
-                if (roundUp(grade) - grade < 3)
-                    finalGrade[i] = roundUp(grade)
-                else
-                    finalGrade[i] = grade
-            }
-        }
-        return finalGrade
-    }
-
-
-    fun roundUp(n: Int): Int {
-        return (n + 4) / 5 * 5
+        assertEquals(GradingStudents.expectedArray, GradingStudents.inputArray)
     }
 }
